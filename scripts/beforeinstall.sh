7f958984fcb852542801baf28c041e82f839fcb1
Ubuntu <ubuntu@ip-172-31-12-8.ap-south-1.compute.internal>
2018-11-25 08:29:27 +0000

    install java script

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+
 sudo service springboot stop
 
 # create springboot service
