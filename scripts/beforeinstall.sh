1901ca22ee2b3c6b81f670809c1341f7ab244d82
Ubuntu <ubuntu@ip-172-31-12-8.ap-south-1.compute.internal>
2018-11-25 08:08:43 +0000

    codedeploy

---
+++ scripts/beforeinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo service springboot stop
+
+# create springboot service
+cat > /etc/init/springboot.conf <<'EOF'
+
+description "springboot Server"
+
+  start on runlevel [2345]
+  stop on runlevel [!2345]
+  respawn
+  respawn limit 10 5
+
+  # run as non privileged user 
+  # add user with this command:
+  ## adduser --system --ingroup www-data --home /opt/apache-tomcat apache-tomcat
+  # Ubuntu 12.04: (use 'exec sudo -u apache-tomcat' when using 10.04)
+  setuid ubuntu
+  setgid ubuntu
+
+  # adapt paths:
+  
+  exec java -jar /home/ubuntu/deploy/spring-boot-web-0.0.1-SNAPSHOT.jar  >> /var/log/springboot.log 2>&1
+
+  # cleanup temp directory after stop
+  post-stop script
+  end script
+EOF
+
+# remove old directory
+rm -rf /home/ubuntu/deploy
+
+# create directory deploy
+mkdir -p /home/ubuntu/deploy 
+
+
